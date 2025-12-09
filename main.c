53fb1f6debe33075281eae02f95c32b8908593da
ghost-mann <iamaob8@protonmail.com>
2025-12-09 13:24:47 +0300

    feat: added terminal menu in main.c

--- main.c
+++ main.c
@@ -10,7 +10,39 @@ struct Student students[10];
 int count = 0;
 
 int main() {
-
+    int choice;
     
+    while (1) {
+        printf("\n=== Student Grade Manager ===\n");
+        printf("1. Add Student\n");
+        printf("2. Display All Students\n");
+        printf("3. Show Average Marks\n");
+        printf("4. Show Highest Marks\n");
+        printf("5. Exit\n");
+        printf("Enter choice: ");
+        scanf("%d", &choice);
+
+        if (choice == 1) {
+
+        }
+        else if (choice == 2) {
+
+        }
+        else if (choice == 3) {
+
+        }
+        else if (choice == 4) {
+
+        }
+        else if (choice == 5) {
+            printf("Goodbye!\n");
+
+        }
+        else {
+            printf("Invalid choice!\n");
+        }
+
+    }
+    return 0;
 
 }
